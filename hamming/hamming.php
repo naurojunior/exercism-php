6aaad723060c223bf98605433bb3be130559e731
Nauro <nauro@outlook.com>
2017-08-09 22:26:28 -0300

    Hamming exercise

--- hamming/hamming.php
+++ hamming/hamming.php
@@ -5,9 +5,19 @@
 // convenience to get you started writing code faster.
 //
 
-function distance($a, $b)
-{
-    //
-    // YOUR CODE GOES HERE
-    //
+function distance($dna1, $dna2) {
+
+    if (strlen($dna1) != strlen($dna2)) {
+        throw new InvalidArgumentException();
+    }
+    
+    $diff = 0;
+
+    for ($i = 0; $i < strlen($dna1); $i++) {
+        if($dna1[$i] != $dna2[$i]){
+            $diff++;
+        }
+    }
+    
+    return $diff;
 }
